817e441a588d600b56126f8a516506c173d47848
sonugupta <sonugupta27032001@gmail.com>
2025-05-19 15:46:33 +0530

    learning the routing and advanced routing

--- src/app.js
+++ src/app.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const app = express();
+// app.get(/^\/ab?c$/, (req, res) => {
+//     res.send("this is me");
+//   });
+  
+//here b is optional that is match abc and ac
+// app.get(/^\/ab+c$/, (req, res) => {
+//     res.send("any number of b works except zero");
+//   });
+  
+// app.get(/^\/ab.*cd$/,(req,res)=>{
+//     res.send("anything in between ab and cd you write it get matched")
+// })
+// app.get(/^\/(ab)?c$/, (req, res) => {
+//     res.send("here ab is optional");
+//   });
+// app.get(/a/,(req,res)=>{
+//     res.send("any of the path contain a it will works it is regex")
+// })
+// app.get(/.*fly$/,(req,res)=>{
+//     res.send("it will handle all the route which end on fly")
+// })
+// app.get("/user",(req,res)=>{
+//     console.log(req.query)
+  
+//     res.send("how can we get the params of the url or say how we read the query parameters")
 
-// Route 1
-app.get("/user", (req, res) => {
-    res.send({firstname:"sonu",seondName:"gupta"});
-});
-app.post("/user",(req,res)=>{
-    //db is saving
-    res.send("data is saved successfully")
-})
-app.delete('/user',(req,res)=>{
-    res.send("the user get deleted")
-})
-
-// Route 3
-app.use("/test", (req, res) => {
-    res.send("hello from the server");
+// })
+app.get("/user/:userid/:name", (req, res) => {
+    console.log(req.params);
+    res.send("getting query");
 });
 
 app.listen(3000, () => {
