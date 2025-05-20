128f388082e354e5fa72ff46bbd105ce505b6705
sonugupta <sonugupta27032001@gmail.com>
2025-05-20 16:01:34 +0530

    api level sanitisation

--- src/app.js
+++ src/app.js
@@ -63,11 +63,22 @@ app.delete("/user",async(req,res)=>{
         res.status(400).send("something went wrong")
     }
 })
-app.patch("/user",async(req,res)=>{
-    const userId = req.body.userId;
+app.patch("/user/:userId",async(req,res)=>{
+    const userId = req.params.userId;
     const data  = req.body;
+ 
     
     try{
+        const ALLOWED_UPDATES = ["photoUrl","about","gender","age","skills"];
+        const isUpdateAllowed = Object.keys(data).every((k) => 
+            ALLOWED_UPDATES.includes(k)
+        );
+        if(!isUpdateAllowed){
+           throw new Error("Update not allowed")
+        }
+        if(data?.skills.length>10){
+            throw new Error("skills cannot be greater than 10")
+        }
         //it ignore all field like userId and only update such field which matches with your schema
         //by default it return the before docuemnt
         const user = await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"before", runValidators:true, })
@@ -75,7 +86,7 @@ app.patch("/user",async(req,res)=>{
       
 
     }catch(err){
-        res.status(400).send("something went wrong" + err.message)
+        res.status(400).send("something went wrong " + err.message)
     }
 })
 
